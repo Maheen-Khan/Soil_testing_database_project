aa5968e4863cd30354efd880ba1c635f0a4cf677
Maheen Khan <maheenaminkhan@gmail.com>
2024-09-30 13:21:14 -0400

    organizing

---
+++ Backend/Models/Request.js
@@ -0,0 +1,26 @@
+import mongoose from "./mongoose.js";
+
+const labTestSchema = new mongoose.Schema({
+
+    userID : mongoose.SchemaType.ObjectID(),
+    tests: [{
+        soilTestID: {type:mongoose.SchemaType.ObjectID(), required:True},
+        quantity: {type:Number, required:True},
+        price: {type:Number,required:True}, //Should come from SoilTests.tests
+    }],
+    shippingSlip: String,
+    status: {type:String, default:"Pending"},
+    results: String,
+    payment: {
+        id:String, //Stripe Payment,Indent ID
+        status:String,
+        amount:Number,
+        currency:String
+    },
+
+    createdAt: {Type:Date,default:Date.now()},
+    updatedAt: Date
+
+});
+
+module.exports(labTestSchema.model("labTest"),mongoose.Schema);
